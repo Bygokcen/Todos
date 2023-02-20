554320388e99a474ad122ca64d510d97dd1a241a
Sebahattin Gökcen Özden <47432051+Bygokcen@users.noreply.github.com>
2023-02-20 06:34:12 +0300

    Add files via upload

---
+++ TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoLists.Models
+{
+	public class TodoItem
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage ="Açıklama gerekli !")]
+		public string Description { get; set; }
+		[Required(ErrorMessage ="Durum Belirtilmeli !")]
+		public string Status { get; set; }
+
+       
+    }
+}
+
